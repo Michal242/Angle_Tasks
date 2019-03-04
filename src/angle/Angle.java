fb187125fb5e27ef9a8719524b8209b13eba6b58
michal <m_wojnakowski@o2.pl>
2019-03-04 10:54:51 +0100

    Dodanie rozwiązania zad.3.

--- src/angle/Angle.java
+++ src/angle/Angle.java
@@ -36,7 +36,6 @@ public class Angle {
     }
 
     public double getAngleCosecans() {
-
         return 1.0 / Math.cos(x);
     }
 
@@ -49,7 +48,58 @@ public class Angle {
 
     }
 
+    @Override
+    public String toString() {
 
+        String degree = Double.toString(Math.toDegrees(x));
+        String degreeFinal = degree;
+        String decimalPlaces = "";
+        String minutesString = "0";
+        String secondsString = "0";
+        double minutes;
+        double seconds;
 
+        /* sprawdzenie czy stopnie s¹ liczb¹ zmiennoprzecinkow¹, je¿eli tak to
+            wykonanie odpowiednich kalkulacji w celu wydobycia minut i sekund,
+            jeœli nie stopnie zostaj¹ tak ja s¹.
+        */
 
+        if (degree.contains(".")) {
+            // ------- Obliczenie samych stopni ------
+
+            // wytnij i wez wszysko z przed '.' to bêd¹ stopnie
+
+            degreeFinal = degree.substring(0, degree.indexOf("."));
+
+            // ------ Obliczenie pzosta³ych minut ------
+
+            // wez wszystko za kropk¹ (ze stopni), nastêpnie dodaj '0.' przed, otrzymamy czêœæ
+            // u³amkow¹, nastepnie przemnozymy przez 60 i otrzymamy minuty
+
+            decimalPlaces = degree.substring(degree.indexOf(".") + 1);
+            minutes = Double.parseDouble("0." + decimalPlaces) * 60;
+            minutesString = String.valueOf(minutes);
+
+            // ------ Obliczenie pzosta³ych sekund ------
+
+            // wez wszystko za kropk¹ (z minut), nastêpnie dodaj '0.' przed, otrzymamy czêœæ
+            // u³amkow¹, nastepnie przemnozymy przez 60 i otrzymamy sekundy, wynik zaokr¹glamy
+            // bo nie potrzebujemy czêœci u³¹mkowej z sekund ju¿ do niczego.
+
+            decimalPlaces = minutesString.substring(minutesString.indexOf(".") + 1);
+            seconds = Double.parseDouble("0." + decimalPlaces) * 60;
+            secondsString = String.valueOf(Math.round(seconds));
+
+            // tu odrzuæ jeszcze czêœæ u³amkow¹ z minut, bierz tylko ca³kowite wartoœci,
+            //    reszta (czêœæ u³amkowa) zosta³a przekonwertowana na sekundy
+            minutesString = minutesString.substring(0, minutesString.indexOf("."));
+        }
+        return "\nK¹t = " + degreeFinal + "° " + minutesString + "' " + secondsString + "\"";
+
+                /*+"\n" +
+                "Angle in \" = " + ((Math.toDegrees(x) * 3600)) + "\n" +
+                degreeFinal;
+
+*/
+    }
 }
--- src/angle/MainAngle.java
+++ src/angle/MainAngle.java
@@ -4,17 +4,19 @@ public class MainAngle {
 
     public static void main(String[] args) {
 
-        Angle angle = new Angle(3.14);
+        Angle angle = new Angle(11);
         double angleValueRad = angle.getAngle();
         System.out.println("K¹t w radianach: " + angle.radian());
         System.out.println("K¹t w stopniach: " + angle.degree());
-        System.out.println("\nWartoœci wybranych funkcji trygonometrycznych.");
-        System.out.printf("Sinus(%.2f) = %.20f\n", angleValueRad, angle.getAngleSinus());
-        System.out.printf("Cosinus(%.2f) = %.20f\n", angleValueRad, angle.getAngleCosiunus());
-        System.out.printf("Tangens(%.2f) = %.20f\n", angleValueRad, angle.getAngleTangens());
-        System.out.printf("Cotangens(%.2f) = %.20f\n", angleValueRad, angle.getAngleCotangens());
-        System.out.printf("Secans(%.2f) = %.20f\n", angleValueRad, angle.getAngleSecans());
-        System.out.printf("Cosecans(%.2f) = %.20f\n", angleValueRad, angle.getAngleCosecans());
+        System.out.println("\nWartoœci wybranych funkcji trygonometrycznych.\n");
+        System.out.printf("Sinus(%.2f) = %.4f\n", angleValueRad, angle.getAngleSinus());
+        System.out.printf("Cosinus(%.2f) = %.4f\n", angleValueRad, angle.getAngleCosiunus());
+        System.out.printf("Tangens(%.2f) = %.4f\n", angleValueRad, angle.getAngleTangens());
+        System.out.printf("Cotangens(%.2f) = %.4f\n", angleValueRad, angle.getAngleCotangens());
+        System.out.printf("Secans(%.2f) = %.4f\n", angleValueRad, angle.getAngleSecans());
+        System.out.printf("Cosecans(%.2f) = %.4f\n", angleValueRad, angle.getAngleCosecans());
+
+        System.out.println(angle);
 
 
     }
