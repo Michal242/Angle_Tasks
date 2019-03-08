dd5ed6af2bb0122da77b72e0b04d998cc6bdd57b
michal <m_wojnakowski@o2.pl>
2019-03-08 10:58:49 +0100

    Dodanie rozwiązania zad.10

---
+++ src/angle/MainAngleZad10.java
@@ -0,0 +1,31 @@
+package angle;
+
+public class MainAngleZad10 {
+
+    public static void main(String[] args) {
+
+        Angle angleFirst = new Angle(Math.PI);
+        Angle angleSecond = new Angle(2 * Math.PI);
+
+        System.out.println("Wartoœci bazowe k¹tów;");
+        System.out.println("AngleFirst : " + " radians [" + angleFirst.radian() + "] ,degrees [" + angleFirst.degree() + "]");
+        System.out.println("AngleSecond : " + " radians [" + angleSecond.radian() + "] ,degrees [" + angleSecond.degree() + "]");
+        System.out.println();
+        System.out.println("Wywo³anie metody dodawania add() - powstaje nowy obiekt Angle angleAdded");
+
+
+        Angle angleAdded = angleFirst.add((angleSecond));
+        System.out.println(angleAdded);
+        System.out.println("angleAdded : " + " radians [" + angleAdded.radian() + "] ,degrees [" + angleAdded.degree() + "]");
+
+        System.out.println();
+        System.out.println("Wywo³anie metody odejmowania sub() - powstaje nowy obiekt Angle angleSubtracted");
+        Angle angleSubtratcted = angleFirst.sub((angleSecond));
+        System.out.println(angleSubtratcted);
+        System.out.println("angleAdded : " + " radians [" + angleSubtratcted.radian() + "] ,degrees [" + angleSubtratcted.degree() + "]");
+
+
+
+
+    }
+}
